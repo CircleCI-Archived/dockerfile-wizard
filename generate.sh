adaad62431646188e7d90b1ec0444b61089920ea
iynere <iynere@users.noreply.github.com>
2017-10-02 15:58:50 -0400

    fix java install

--- generate.sh
+++ generate.sh
@@ -45,8 +45,10 @@ RUN if [ \$(grep 'VERSION_ID="8"' /etc/os-release) ] ; then \\
 ; elif [ \$(grep 'VERSION_ID="14.04"' /etc/os-release) ] ; then \\
 		apt-get update && \\
     apt-get --force-yes -y install software-properties-common python-software-properties && \\
-    echo | add-apt-repository ppa:webupd8team/java && \\
+    echo | add-apt-repository -y ppa:webupd8team/java && \\
     apt-get update && \\
+    echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
+    echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
     apt-get -y install oracle-java8-installer \\
 ; else \\
     apt-get -y install openjdk-8-jdk \\
