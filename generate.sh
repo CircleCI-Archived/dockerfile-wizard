dfd492c1d1618e847b38f6aa59caaa571d9d90f6
iynere <iynere@users.noreply.github.com>
2017-11-06 17:28:21 -0500

    chromedriver jessie

--- generate.sh
+++ generate.sh
@@ -103,4 +103,13 @@ echo "RUN curl --silent --show-error --location --fail --retry 3 --output /tmp/g
        \"/opt/google/chrome/google-chrome\" \
   && google-chrome --version"
 
+echo "RUN export CHROMEDRIVER_RELEASE=$(curl --location --fail --retry 3 http://chromedriver.storage.googleapis.com/LATEST_RELEASE) \
+  && curl --silent --show-error --location --fail --retry 3 --output /tmp/chromedriver_linux64.zip \"http://chromedriver.storage.googleapis.com/$CHROMEDRIVER_RELEASE/chromedriver_linux64.zip\" \
+  && cd /tmp \
+  && unzip chromedriver_linux64.zip \
+  && rm -rf chromedriver_linux64.zip \
+  && mv chromedriver /usr/local/bin/chromedriver \
+  && chmod +x /usr/local/bin/chromedriver \
+  && chromedriver --version"
+
 fi
