1a88bc6e61d25b67b5cc0a07a64484aeee753446
iynere <iynere@users.noreply.github.com>
2017-11-06 16:34:28 -0500

    like this?

--- generate.sh
+++ generate.sh
@@ -89,13 +89,5 @@ RUN if [ \$(grep 'VERSION_ID="8"' /etc/os-release) ] ; then \\
 EOF
 echo "# start xvfb automatically to avoid needing to express in circle.yml
 ENV DISPLAY :99
-RUN printf '#!/bin/sh\nXvfb :99 -screen 0 1280x1024x24 &\nexec \"$@\"\n' > /tmp/entrypoint \
-	&& chmod +x /tmp/entrypoint \
-        && mv /tmp/entrypoint /docker-entrypoint.sh
-
-# ensure that the build agent doesn't override the entrypoint
-LABEL com.circleci.preserve-entrypoint=true
-
-ENTRYPOINT [\"/docker-entrypoint.sh\"]
-CMD [\"/bin/sh\"]"
+CMD Xvfb :99 -screen 0 1280x1024x24"
 fi
