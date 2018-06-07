dcefdf0de9909d4cce894e4df9d522f61fed3012
KP <kkp@fender.com>
2018-06-07 15:32:34 -0700

    Removing sudo

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -45,8 +45,8 @@ export PATH=\"$HOME/.tfenv/bin:$PATH\" && \
 tfenv install latest"
 
 echo "RUN apt-get install -y python2.7 && \
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1 && \
-sudo apt-get -y install python-simplejson python-minimal aptitude python-pip python-dev && \
+update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1 && \
+apt-get -y install python-simplejson python-minimal aptitude python-pip python-dev && \
 pip install --upgrade pip && \
 pip install google_compute_engine boto boto3 botocore six awscli 'ansible==2.5.0'"
 # Install Ansible
