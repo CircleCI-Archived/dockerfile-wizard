d58156b7e06c6eecaabb70787d6c2b631606cef5
KP <kkp@fender.com>
2018-06-07 16:42:43 -0700

    Added more dependencies

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -37,7 +37,19 @@ fi
 
 ## Fender-specific items ##
 
-echo "RUN apt-get install -y unzip sudo rsync parallel"
+echo "RUN apt-get install -y unzip sudo rsync parallel tar jq"
+
+# Install Golang
+echo "RUN wget https://storage.googleapis.com/golang/go$GOVERS.linux-amd64.tar.gz && \
+tar -xzf go$GOVERS.linux-amd64.tar.gz && \
+mv go /usr/local/go$GOVERS && \
+rm go$GOVERS.linux-amd64.tar.gz && \
+go get golang.org/x/tools/cmd/cover && \
+go get github.com/mattn/goveralls && \
+\(wget -q -O honeymarker https://honeycomb.io/download/honeymarker/linux/1.9 && \
+      echo 'e74514a2baaf63a5828ff62ca2ca1aa86b3a4ab223ab6a7c53f969d7b55e37fb  honeymarker' | sha256sum -c && \
+      chmod 755 ./honeymarker && \
+      sudo mv honeymarker /usr/bin\)"
 
 # Install latest version of Terraform
 echo "RUN git clone https://github.com/kamatama41/tfenv.git $HOME/.tfenv && \
