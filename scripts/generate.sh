49b479c281526f9c4d00c385d19961ab7389cbe0
KP <kkp@fender.com>
2018-06-12 10:24:15 -0700

    Install java before installing Elasticsearch

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -35,6 +35,33 @@ if [ ! -e $PYTHON_VERSION_NUM ] ; then
     make install"
 fi
 
+if [ $JAVA = "true" ] ; then
+cat << EOF
+RUN if [ \$(grep 'VERSION_ID="8"' /etc/os-release) ] ; then \\
+    echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list && \\
+    apt-get update && apt-get -y install -t jessie-backports openjdk-8-jdk ca-certificates-java \\
+; elif [ \$(grep 'VERSION_ID="9"' /etc/os-release) ] ; then \\
+    apt-get update && apt-get -y -q --no-install-recommends install -t stable openjdk-8-jdk ca-certificates-java \\
+; elif [ \$(grep 'VERSION_ID="14.04"' /etc/os-release) ] ; then \\
+    apt-get update && \\
+    apt-get --force-yes -y install software-properties-common python-software-properties && \\
+    echo | add-apt-repository -y ppa:webupd8team/java && \\
+    apt-get update && \\
+    echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
+    echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
+    apt-get -y install oracle-java8-installer \\
+; elif [ \$(grep 'VERSION_ID="16.04"' /etc/os-release) ] ; then \\
+    apt-get update && \\
+    apt-get --force-yes -y install software-properties-common python-software-properties && \\
+    echo | add-apt-repository -y ppa:webupd8team/java && \\
+    apt-get update && \\
+    echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
+    echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
+    apt-get -y install oracle-java8-installer \\
+; fi
+EOF
+fi
+
 ## Fender-specific items ##
 
 echo "RUN apt-get install -y unzip rsync parallel tar jq wget"
@@ -82,33 +109,6 @@ apt-get update && apt-get -y install elasticsearch && \
 # if [ ! -e $PHP_VERSION_NUM ] ; then
 #     wget "http://php.net/distributions/php-${PHP_VERSION_NUM}.tar.xz"
 # fi
-
-if [ $JAVA = "true" ] ; then
-cat << EOF
-RUN if [ \$(grep 'VERSION_ID="8"' /etc/os-release) ] ; then \\
-    echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list && \\
-    apt-get update && apt-get -y install -t jessie-backports openjdk-8-jdk ca-certificates-java \\
-; elif [ \$(grep 'VERSION_ID="9"' /etc/os-release) ] ; then \\
-		apt-get update && apt-get -y -q --no-install-recommends install -t stable openjdk-8-jdk ca-certificates-java \\
-; elif [ \$(grep 'VERSION_ID="14.04"' /etc/os-release) ] ; then \\
-		apt-get update && \\
-    apt-get --force-yes -y install software-properties-common python-software-properties && \\
-    echo | add-apt-repository -y ppa:webupd8team/java && \\
-    apt-get update && \\
-    echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
-    echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
-    apt-get -y install oracle-java8-installer \\
-; elif [ \$(grep 'VERSION_ID="16.04"' /etc/os-release) ] ; then \\
-    apt-get update && \\
-    apt-get --force-yes -y install software-properties-common python-software-properties && \\
-    echo | add-apt-repository -y ppa:webupd8team/java && \\
-    apt-get update && \\
-    echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
-    echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
-    apt-get -y install oracle-java8-installer \\
-; fi
-EOF
-fi
 
 if [ $MYSQL_CLIENT = "true" ] ; then
     echo "RUN apt-get -y install mysql-client"
